fc75947e4b286a53d787728aeb8302d665811a07
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2025-03-01 12:12:17 +0530

    basic model done

--- home/models.py
+++ home/models.py
@@ -10,4 +10,17 @@ class Event(models.models):
     capacity = models.IntegerField()
     ticket_price = models.FloatField(default = 250)
     status = models.CharField(max_length=255,default=(('Upcoming','Upcoming'),('Cancled','Cancled'),('Happning','Happning')))
+    image = models.ImageField(upload_to='event_images',blank=True,null=True)
 
+class Ticket(models.Model):
+    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    ticket_type = models.CharField(max_length=255,choices=('VIP','VIP'))
+    price = models.FloatField()
+
+class Booking(models.Model):
+    ticket = models.ForeignKey(Ticket,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    status = models.CharField(max_length=255,default='Pending')
+    total_price = models.FloatField()
+    ticket_type = models.CharField(max_length=255,choices=(('VIP','VIP')))
+    
